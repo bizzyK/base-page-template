c210191b563169e765fd8c1991ac2f8e79bf0f70
Michael Nau <michaelwnau@gmail.com>
2024-07-16 12:12:28 -0500

    Add README for Storybook instructions inside stories directory

--- portal/src/stories/portalHeader.stories.ts
+++ portal/src/stories/portalHeader.stories.ts
@@ -2,4 +2,4 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { within, userEvent, expect } from '@storybook/test';
 import { fn } from '@storybook/test';
 
-import { portalHeader } from './portalHeader';
+import { Navbar } from '../components/navbar/Navbar';
